2298a0f16d2c9d82e66de4913484cd4df25c974a
KhineWin <khinewin123@gmail.com>
2021-07-02 08:32:13 +0630

    paginate 40

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,7 +10,7 @@ class WelcomeController extends Controller
 {
     public function getFoodsList(){
         //$cats=Cat::with('foodpairs')->get();
-        $foods=Foodpair::OrderBy('id', 'desc')->paginate("20");
+        $foods=Foodpair::OrderBy('id', 'desc')->paginate("40");
         return view ('foods-list')->with(['foods'=>$foods]);
 
     }
@@ -22,7 +22,7 @@ class WelcomeController extends Controller
     public function getSearchResult(Request $request){
         $q=$request['q'];
         $foods=Foodpair::where('first_food_name', 'LIKE', "%$q%")->orWhere('second_food_name', "LIKE", "%$q%")
-        ->OrderBy('id', 'desc')->paginate("20");
+        ->OrderBy('id', 'desc')->paginate("40");
         return view ('search-foods')->with(['foods'=>$foods, 'q'=>$q]);
     }
 }
